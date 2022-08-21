1a486f395c5cd362e74995baa94aebe82e3b60b8
stupns <stupns@i.ua>
2022-08-21 22:33:57 +0300

    new b

--- blog/config.py
+++ blog/config.py
@@ -7,3 +7,6 @@ BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or '\q\xbfq\x97\xbf\t}\xec\x8c/\xcd\xb5V\x0e\x93\x81\xa0\xa9\xd0I=s\x17'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                              'sqlite:///' + os.path.join(BASEDIR, 'flask.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
