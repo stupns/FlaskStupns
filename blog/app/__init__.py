1ac2b73e23a0513eaa8b55b80d5e6c0b5c5ff2e3
stupns <stupns@i.ua>
2022-08-23 12:32:22 +0300

    completed branch Profile

--- blog/app/__init__.py
+++ blog/app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_login import LoginManager
 from app.routes import main_rout
 from config import Config
 
@@ -10,6 +10,9 @@ app.register_blueprint(main_rout)
 app.config.from_object(Config)
 
 db = SQLAlchemy(app)
+
+login = LoginManager(app)
+login.login_view = 'main_rout.login'
 migrate = Migrate(app, db)
 
-from . import models
+from app import models, routes
--- blog/app/routes.py
+++ blog/app/routes.py
@@ -1,6 +1,10 @@
-from flask import Blueprint, render_template, flash, redirect, url_for
+from datetime import datetime
 
-from app.forms import LoginForm
+from flask import Blueprint, render_template, flash, redirect, url_for, request
+from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.urls import url_parse
+
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 
 main_rout = Blueprint('main_rout', __name__,
                       template_folder='templates',
@@ -9,8 +13,8 @@ main_rout = Blueprint('main_rout', __name__,
 
 @main_rout.route('/')
 @main_rout.route('/index')
+@login_required
 def index():
-    user = {'username': 'Serhii'}
     posts = [
         {
             'author': {'username': 'Serhii'},
@@ -21,14 +25,87 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home', posts=posts)
 
 
 @main_rout.route('/login', methods=['GET', 'POST'])
 def login():
+    from app.models import User
+    if current_user.is_authenticated:
+        return redirect(url_for('main_rout.index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('main_rout.index'))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('main_rout.login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('main_rout.index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
+
+
+@main_rout.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('main_rout.index'))
+
+
+@main_rout.route('/register', methods=['GET', 'POST'])
+def register():
+    from app import db
+    from app.models import User
+
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('main_rout.login'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@main_rout.route('/user/<username>')
+@login_required
+def user(username):
+    from app.models import User
+
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
+
+
+@main_rout.before_request
+def before_request():
+    from app import db
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
+
+
+@main_rout.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    from app import db
+
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('main_rout.edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile',
+                           form=form)
