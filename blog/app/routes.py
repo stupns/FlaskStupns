886ebd43fb5cbe0e19253ca8335505626260a394
stupns <stupns@i.ua>
2022-08-25 16:28:48 +0300

    update branch-pagination

--- blog/app/routes.py
+++ blog/app/routes.py
@@ -4,36 +4,53 @@ from flask import Blueprint, render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 
-from app.forms import LoginForm, RegistrationForm, EditProfileForm, EmptyForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, EmptyForm, PostForm
 
 main_rout = Blueprint('main_rout', __name__,
                       template_folder='templates',
                       static_folder='static')
 
 
-@main_rout.route('/')
-@main_rout.route('/index')
+# region index
+@main_rout.route('/', methods=['GET', 'POST'])
+@main_rout.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
-    posts = [
-        {
-            'author': {'username': 'Serhii'},
-            'body': 'Beautiful day in Ukraine!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('index.html', title='Home', posts=posts)
+    from app import app
+    from app import db
+    from app.models import Post
+
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('main_rout.index'))
+    page = request.args.get('page', 1, type=int)
+    posts = current_user.followed_posts().paginate(
+        page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('main_rout.index', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('main_rout.index', page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('index.html', title='Home', form=form,
+                           posts=posts.items, next_url=next_url,
+                           prev_url=prev_url)
 
 
+# endregion
+# region login
 @main_rout.route('/login', methods=['GET', 'POST'])
 def login():
     from app.models import User
+
+    # Визначити, чи підтверджений цей користувач, і повернутися на домашню сторінку, якщо він підтверджений.
     if current_user.is_authenticated:
         return redirect(url_for('main_rout.index'))
+
     form = LoginForm()
+    # Перевірка даних таблиці
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
@@ -47,12 +64,16 @@ def login():
     return render_template('login.html', title='Sign In', form=form)
 
 
+# endregion
+# region logout
 @main_rout.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('main_rout.index'))
 
 
+# endregion
+# region register
 @main_rout.route('/register', methods=['GET', 'POST'])
 def register():
     from app import db
@@ -71,28 +92,30 @@ def register():
     return render_template('register.html', title='Register', form=form)
 
 
+# endregion
+# region user
 @main_rout.route('/user/<username>')
 @login_required
 def user(username):
+    from app import app
     from app.models import User
+    from app.models import Post
 
     user = User.query.filter_by(username=username).first_or_404()
-    posts = [
-        {'author': user, 'body': 'Test post #1'},
-        {'author': user, 'body': 'Test post #2'}
-    ]
+    page = request.args.get('page', 1, type=int)
+    posts = user.posts.order_by(Post.timestamp.desc()).paginate(
+        page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('user', username=user.username, page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('user', username=user.username, page=posts.prev_num) \
+        if posts.has_prev else None
     form = EmptyForm()
-    return render_template('user.html', user=user, posts=posts, form=form)
+    return render_template('user.html', user=user, posts=posts.items,
+                           next_url=next_url, prev_url=prev_url, form=form)
 
 
-@main_rout.before_request
-def before_request():
-    from app import db
-    if current_user.is_authenticated:
-        current_user.last_seen = datetime.utcnow()
-        db.session.commit()
-
-
+# endregion
+# region profile
 @main_rout.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
 def edit_profile():
@@ -112,6 +135,8 @@ def edit_profile():
                            form=form)
 
 
+# endregion
+# region follow
 @main_rout.route('/follow/<username>', methods=['POST'])
 @login_required
 def follow(username):
@@ -135,6 +160,8 @@ def follow(username):
         return redirect(url_for('main_rout.index'))
 
 
+# endregion
+# region unfollow
 @main_rout.route('/unfollow/<username>', methods=['POST'])
 @login_required
 def unfollow(username):
@@ -146,13 +173,42 @@ def unfollow(username):
         user = User.query.filter_by(username=username).first()
         if user is None:
             flash('User {} not found.'.format(username))
-            return redirect(url_for('index'))
+            return redirect(url_for('main_rout.index'))
         if user == current_user:
             flash('You cannot unfollow yourself!')
-            return redirect(url_for('user', username=username))
+            return redirect(url_for('main_rout.user', username=username))
         current_user.unfollow(user)
         db.session.commit()
         flash('You are not following {}.'.format(username))
-        return redirect(url_for('user', username=username))
+        return redirect(url_for('main_rout.user', username=username))
     else:
-        return redirect(url_for('index'))
+        return redirect(url_for('main_rout.index'))
+
+
+# endregion
+# region explore
+@main_rout.route('/explore')
+@login_required
+def explore():
+    from app.models import Post
+    from app import app
+
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('main_rout.explore', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('main_rout.explore', page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template("index.html", title='Explore', posts=posts.items,
+                           next_url=next_url, prev_url=prev_url)
+
+
+# endregion
+
+@main_rout.before_request
+def before_request():
+    from app import db
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
